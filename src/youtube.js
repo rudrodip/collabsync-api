7f6a85c0fca48e53a7cdffb41a2c4ab42e19cf6a
rudrodip <official.rudrodipsarker@gmail.com>
2023-09-12 20:44:15 +0600

    added more functions

--- src/youtube.js
+++ src/youtube.js
@@ -13,7 +13,10 @@ class Youtube {
    */
   constructor(userAccessToken, refreshToken) {
     this.access_token = userAccessToken;
-    this.scopes = ["https://www.googleapis.com/auth/youtube"];
+    this.scopes = [
+      "https://www.googleapis.com/auth/youtube",
+      "https://www.googleapis.com/auth/youtube.upload",
+    ];
 
     /**
      * OAuth2 client for YouTube API.
@@ -37,6 +40,15 @@ class Youtube {
     this.youtube = google.youtube({
       version: "v3",
       auth: this.oauth2Client,
+    });
+
+    /**
+     * YouTube Analytics API client instance.
+     * @type {google.youtubeAnalytics}
+     */
+    this.youtubeAnalytics = google.youtubeAnalytics({
+      version: "v2",
+      auth: this.oAuth2Client
     });
   }
 
@@ -82,27 +94,6 @@ class Youtube {
   }
 
   /**
-   * Get a list of user's playlists.
-   * @returns {Promise<Object[]>} - Array of playlists.
-   */
-  async getMyPlaylists() {
-    try {
-      const response = await this.youtube.playlists.list({
-        auth: this.oauth2Client,
-        part: "snippet",
-        mine: true,
-        maxResults: 10,
-      });
-
-      const playlists = response.data.items;
-      return playlists;
-    } catch (error) {
-      console.error("Error retrieving playlists:", error);
-      throw error;
-    }
-  }
-
-  /**
    * List user's YouTube channels.
    * @returns {Promise<Object[]>} - Array of user's channels.
    */
@@ -123,6 +114,88 @@ class Youtube {
       return response.data.items;
     } catch (error) {
       console.error("Error listing user's channels:", error);
+      throw error;
+    }
+  }
+
+  /**
+   * Get channel data by channel ID.
+   * @param {string} channelId - YouTube channel ID.
+   * @returns {Promise<Object>} - Channel data.
+   */
+  async getChannelById(channelId) {
+    try {
+      const response = await this.youtube.channels.list({
+        auth: this.oauth2Client,
+        part: "snippet",
+        id: channelId,
+      });
+
+      if (response.data.items.length > 0) {
+        const channelData = response.data.items[0];
+        return channelData;
+      } else {
+        console.log("Channel not found");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error getting channel data:", error);
+      throw error;
+    }
+  }
+
+  /**
+   * Get channel analytics data.
+   * @param {string} channelId - The ID of the channel to retrieve analytics for.
+   * @returns {Promise<Object>} - Channel analytics data.
+   */
+  async getChannelAnalytics(channelId, startDate="2023-01-01") {
+    try {
+      const response = await this.youtubeAnalytics.reports.query({
+        auth: this.oauth2Client,
+        ids: `channel==${channelId}`,
+        startDate: startDate,
+        endDate: new Date().toISOString().slice(0, 10),
+        metrics: "views,comments,likes,dislikes,shares",
+        dimensions: 'day',
+        sort: 'day'
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error("Error retrieving channel analytics:", error);
+      throw error;
+    }
+  }
+
+  /**
+   * Get the best-performing videos for a channel.
+   * @param {string} channelId - The ID of the channel to retrieve videos for.
+   * @param {number} maxResults - The maximum number of videos to retrieve.
+   * @returns {Promise<Object[]>} - Array of best-performing videos.
+   */
+  async getBestPerformingVideos(channelId, maxResults = 10) {
+    try {
+      const response = await this.youtube.search.list({
+        auth: this.oauth2Client,
+        part: "id",
+        channelId,
+        maxResults,
+        order: "viewCount",
+        type: "video",
+      });
+
+      const videoIds = response.data.items.map((item) => item.id.videoId);
+
+      const videoDetailsResponse = await this.youtube.videos.list({
+        auth: this.oauth2Client,
+        part: "snippet,statistics",
+        id: videoIds.join(","),
+      });
+
+      return videoDetailsResponse.data.items;
+    } catch (error) {
+      console.error("Error retrieving best-performing videos:", error);
       throw error;
     }
   }
